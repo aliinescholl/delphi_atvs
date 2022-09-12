af68b992caf1bf8c87ced53fb54f90f5dd8fc5cb
aliinescholl <76217058+aliinescholl@users.noreply.github.com>
2022-09-12 18:21:52 -0300

    Exercícios aula 09/09/2022

---
+++ exercicio6 - for/soma_numeros.dpr
@@ -0,0 +1,42 @@
+program soma_numeros;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+  xQtdEntrada: Integer;
+  xNumero: Integer;
+  I: Integer;
+  xSoma: Integer;
+begin
+  try
+  xSoma := 0;
+
+  WriteLn('Digite o total de números:');
+  ReadLn(xQtdEntrada);
+
+  for I := 0 to xQtdEntrada-1 do
+  begin
+    WriteLn('Entre com o ' + IntToStr(I+1) + ' nº: ');
+    ReadLn(xNumero);
+
+  {for I := xQtdEntrada downto 1 do
+  begin
+    WriteLn('Entre com o ' + IntToStr(I+1) + ' nº: ');
+    ReadLn(xNumero);}
+
+    xSoma := xSoma + xNumero;
+  end;
+
+  WriteLn('A soma é: ' + IntToStr(xSoma));
+  ReadLn;
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
