14186915dd547deeb284bab0af266396acc4c5e7
aliinescholl <76217058+aliinescholl@users.noreply.github.com>
2022-09-12 21:45:41 -0300

    Exercícios aula dia 12/09/2022

---
+++ exercicio9.1/Project2.dpr
@@ -0,0 +1,36 @@
+program Project2;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+var
+  x,y : Integer;
+
+begin
+  try
+    x := 5;
+    y := 0;
+
+    while (x > 2) do
+    begin
+      WriteLn(x);
+
+      //if x = 4 then continue; cuidado para nao causar um loop infinito
+
+      y := y + x;
+      x := x - 1;
+
+      //if x = 3 then break;
+      
+    end;
+
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
