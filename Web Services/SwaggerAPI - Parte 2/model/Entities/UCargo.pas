60815750ed107760e4203e8f5ca0eaceb6e9a56a
MySql Dados <mysql@furb.br>
2023-02-07 21:53:13 -0300

    Swagger API

---
+++ Web Services/SwaggerAPI - Parte 2/model/Entities/UCargo.pas
@@ -0,0 +1,29 @@
+unit UCargo;
+
+interface
+
+uses
+    GBSwagger.Model.Attributes;
+
+type
+ TCargo = class
+  private
+    FID: Integer;
+    FDescricao: String;
+  public
+  [SwagProp('Cargo Id', True)]
+    property Id:Integer read FID write FID;
+
+  [SwagProp('Descrição cargo', True)]
+    property Nome: String read FDescricao write FDescricao;
+end;
+  TAPIError = class
+    Private
+      Ferror: String;
+    public
+      property Error: String read FError write FError;
+  end;
+
+implementation
+
+end.
